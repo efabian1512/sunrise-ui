0729dd39ccb8d1166aee9ba5733fd6fb404129e9
efabian1512 <efabian1989@hotmail.com>
2023-11-12 21:39:31 -0400

    feat(sunrise): Setting up contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NavBarComponent,
     ServicesComponent,
     AboutComponent,
-    ContactsComponent
+    ContactsComponent,
   ],
   imports: [
   BrowserModule,
